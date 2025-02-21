5750ed77f3cb50032ddbcafd5cebc3fd8099e735
Arrenius94 <arrenius94@gmail.com>
2025-02-20 23:33:03 -0300

    FEATURE: Listando produtos

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+const ProdcutPage = () => {
+    return <div className="p-5 border border-red-400 rounded-xl">
+        <h1 className="text-red-500">Product Page</h1>
+        <Button>FSW 7.0</Button>
+        <Input placeholder="oioioi"></Input>
+    </div>
+}
+
+export default ProdcutPage
